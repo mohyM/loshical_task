5b889fc1940789e6b518ef7e25a06ea8199b39a4
mohym <a.mohy881@gmail.com>
2023-08-09 00:06:49 +0300

    rest answerimage

--- lib/src/widgets/answer_image.dart
+++ lib/src/widgets/answer_image.dart
@@ -1,34 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:loshical/src/widgets/hugged_child.dart';
 
-class AnswerImage extends StatefulWidget {
-  const AnswerImage(
-      {super.key, required this.assetPath});
+class AnswerImage extends StatelessWidget {
+  const AnswerImage({super.key, required this.assetPath});
   final String assetPath;
-
-
-
-  @override
-  State<AnswerImage> createState() => _AnswerImageState();
-}
-
-class _AnswerImageState extends State<AnswerImage> {
-  GlobalKey containerKey = GlobalKey();
-
-
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-
-  }
 
   @override
   Widget build(BuildContext context) {
     return HuggedChild(
-      key: containerKey,
-      child: Image.asset(widget.assetPath),
+      child: Image.asset(assetPath),
     );
   }
 }
